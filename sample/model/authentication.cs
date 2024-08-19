f06a5e0ff9b0afbe865e65888741d2942d511eab
Zoggle Team OJT <webdevteam2@vctnads.com>
2024-08-19 16:59:06 +0800

    Update submodule reference for BusinessSuite

---
+++ sample/model/authentication.cs
@@ -0,0 +1,10 @@
+namespace sample.model
+{
+    public class Authentication
+    {
+        public int Id { get; set; }
+        public string? Username { get; set;}
+        public string? Password { get; set;}
+    }
+} 
+
